5468346e8d02f05187eefcb272568f773f6ab962
samanthacardosoo <sassacardosoo@gmail.com>
2020-10-21 15:00:33 -0300

    correção de erro de css

--- src/views/admin.js
+++ src/views/admin.js
@@ -3,6 +3,8 @@ const admin = (part1, part2) => {
     <!DOCTYPE html>
     <html lang="pt-br">
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
             <title> Parcial de Votos </title>
             <style>
@@ -19,6 +21,7 @@ const admin = (part1, part2) => {
             
             .participantes {
                 display:flex; 
+                flex-wrap:wrap;
                 justify-content:center
             }
             
--- src/views/index.js
+++ src/views/index.js
@@ -8,7 +8,7 @@ const page = `
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="../static/css/index.css">
-    <title>Big Little Brother</title>
+    <title>Votação</title>
 
 </head>
 
